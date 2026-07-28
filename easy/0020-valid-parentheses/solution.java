0c07e107ba5a650102f05014d8e7b8669e69883a
Karthig Rajan S <rajankarthig@gmail.com>
2026-07-28 14:07:28 +0530

    ✅ Valid Parentheses | Easy | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-07-28

---
+++ easy/0020-valid-parentheses/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n=nums.length;
+        if(n==0){
+            return 0;
+        }
+        int k=0 ;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j] != nums[k]){
+                k++;
+                nums[k] = nums[j];
+            }
+        }
+        return k+1;
+    }
+}
