78fa72d50c0df03338298d5ebfbbe40f2f3cc3c1
Karthig Rajan S <rajankarthig@gmail.com>
2026-07-28 14:07:42 +0530

    ✅ Valid Parentheses | Easy | Java | Runtime: 2 ms (98.17%) | Memory: 43.5 MB | 2026-07-28

--- easy/0020-valid-parentheses/solution.java
+++ easy/0020-valid-parentheses/solution.java
@@ -1,16 +1,17 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int n=nums.length;
-        if(n==0){
-            return 0;
-        }
-        int k=0 ;
-        for(int j=0;j<nums.length;j++){
-            if(nums[j] != nums[k]){
-                k++;
-                nums[k] = nums[j];
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='('){
+                stack.push(')');
+            }else if(c=='{'){
+                stack.push('}');
+            }else if(c=='['){
+                stack.push(']');
+            }else if(stack.isEmpty()|| stack.pop()!=c){
+                return false;
             }
         }
-        return k+1;
+        return stack.isEmpty();
     }
 }
