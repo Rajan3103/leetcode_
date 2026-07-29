995bafd2bad27937b502ee78b22e5621d7c3e37d
Karthig Rajan S <rajankarthig@gmail.com>
2026-07-29 11:40:08 +0530

    ✅ Smallest Palindromic Rearrangement II | Hard | Java | Runtime: 17 ms (83.87%) | Memory: 47.2 MB | 2026-07-29

--- hard/3518-smallest-palindromic-rearrangement-ii/solution.java
+++ hard/3518-smallest-palindromic-rearrangement-ii/solution.java
@@ -1,19 +1,70 @@
-                    }
-                        half[c]++;
-                        k -= ways;
-                    } else {
-                        break;
-                        first.append((char) (c + 'a'));
-                    if(ways >= k) {
+class Solution {
+    public String smallestPalindrome(String s, int k) {
+        int[]freq=new int[26];
+        for(char c : s.toCharArray()){
+            freq[c-'a']++;
+        }
+        int[]half=new int[26];
+        StringBuilder mid=new StringBuilder();
+        int m=0;
 
-                    long ways=getWays(half,k);
-                    half[c]--;
-            for(int c=0;c<26;++c){
-                if(half[c]>0){
-                }
-            }
-        }
-        
-        StringBuilder res = new StringBuilder(first);
-        res.append(mid);
-        res.append(first.reverse());
+        for(int i=0;i<26;i++){
+            if(freq[i]%2!=0){
+                mid.append((char)(i+'a'));
+            }
+            half[i]=freq[i]/2;
+            m+=half[i];
+        }
+        if(getWays(half,k)<k){
+            return "";
+        }
+        StringBuilder first=new StringBuilder();
+        for(int i=0;i<m;i++){
+            for(int c=0;c<26;++c){
+                if(half[c]>0){
+                    half[c]--;
+                    long ways=getWays(half,k);
+
+                    if(ways >= k) {
+                        first.append((char) (c + 'a'));
+                        break;
+                    } else {
+                        k -= ways;
+                        half[c]++;
+                    }
+                }
+            }
+        }
+        
+        StringBuilder res = new StringBuilder(first);
+        res.append(mid);
+        res.append(first.reverse());
+        return res.toString();
+    }
+    
+    private long getWays(int[] f, long targetK) {
+        long ways = 1;
+        int currLen = 0;
+        for (int count : f) {
+            if (count > 0) {
+                currLen += count;
+                long n = currLen;
+                long r = count;
+                
+                if (r > n - r) r = n - r;
+                long curNCr = 1;
+                
+                for (int i = 1; i <= r; ++i) {
+                    curNCr = curNCr * (n - i + 1) / i;
+                    if (curNCr > targetK) {
+                        curNCr = targetK + 1;
+                        break;
+                    }
+                }
+                ways *= curNCr;
+                if (ways > targetK) return targetK + 1;
+            }
+        }
+        return ways;
+    }
+}
