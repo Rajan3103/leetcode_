de54333e6cde9702bfb46ee4fec88fe1fa20bba0
Karthig Rajan S <rajankarthig@gmail.com>
2026-07-29 11:39:45 +0530

    ✅ Smallest Palindromic Rearrangement II | Hard | Java | Runtime: 0 ms | Memory: 42.3 MB | 2026-07-29

---
+++ hard/3518-smallest-palindromic-rearrangement-ii/solution.java
@@ -0,0 +1,19 @@
+                    }
+                        half[c]++;
+                        k -= ways;
+                    } else {
+                        break;
+                        first.append((char) (c + 'a'));
+                    if(ways >= k) {
+
+                    long ways=getWays(half,k);
+                    half[c]--;
+            for(int c=0;c<26;++c){
+                if(half[c]>0){
+                }
+            }
+        }
+        
+        StringBuilder res = new StringBuilder(first);
+        res.append(mid);
+        res.append(first.reverse());
