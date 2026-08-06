8ad919f22e2ea2646344b3da74835114394c61d0
Karthig Rajan S <rajankarthig@gmail.com>
2026-08-06 18:47:03 +0530

    ✅ Smallest Divisible Digit Product I | Easy | Java | Runtime: 0 ms | Memory: 42.2 MB | 2026-08-06

---
+++ easy/3345-smallest-divisible-digit-product-i/solution.java
@@ -0,0 +1,16 @@
+                product *=temp%10;
+            }
+                temp/=10;
+            if (product%t==0) {
+                return n;
+            }
+            n++;
+        }
+    }
+            while(temp>0){
+
+            int temp=n;
+            int product=1;
+        while(true){
+    public int smallestNumber(int n, int t) {
+class Solution {
