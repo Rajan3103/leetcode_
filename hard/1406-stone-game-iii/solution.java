5375f865b8a78c112ac010348b972ac839a2dca6
Karthig Rajan S <rajankarthig@gmail.com>
2026-08-06 12:55:33 +0530

    ✅ Stone Game III | Hard | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-06

---
+++ hard/1406-stone-game-iii/solution.java
@@ -0,0 +1,16 @@
+            dp[i] = Integer.MIN_VALUE;
+            int sum = 0;
+
+            for (int j = i; j < Math.min(n, i + 3); j++) {
+
+                sum += stoneValue[j];
+
+
+        for (int i = n - 1; i >= 0; i--) {
+
+        int[] dp = new int[n + 1];
+
+        int n = stoneValue.length;
+
+    public String stoneGameIII(int[] stoneValue) {
+class Solution {
