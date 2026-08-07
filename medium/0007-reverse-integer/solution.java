30c03594140007c86e902091784b2a3fea9e5054
Karthig Rajan S <rajankarthig@gmail.com>
2026-08-07 21:31:05 +0530

    ✅ Reverse Integer | Medium | Java | Runtime: 1 ms (99.98%) | Memory: 42.8 MB | 2026-08-07

--- medium/0007-reverse-integer/solution.java
+++ medium/0007-reverse-integer/solution.java
@@ -1,16 +1,18 @@
-            x/=10;
-            if(r>Integer.MAX_VALUE/10||(r == Integer.MAX_VALUE / 10 && digit > 7)){
-                return 0;
-            }
-            if (r < Integer.MIN_VALUE / 10 ||
-                (r == Integer.MIN_VALUE / 10 && digit < -8)) {
-                return 0;
-            }
-        }
-            r=r*10+digit;
-        return r;
-            int digit=x%10;
-        while(x!=0){
-        int r=0;
-class Solution {
-    public int reverse(int x) {
+class Solution {
+    public int reverse(int x) {
+        int r=0;
+        while(x!=0){
+            int digit=x%10;
+            x/=10;
+            if(r>Integer.MAX_VALUE/10||(r == Integer.MAX_VALUE / 10 && digit > 7)){
+                return 0;
+            }
+            if (r < Integer.MIN_VALUE / 10 ||
+                (r == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0;
+            }
+            r=r*10+digit;
+        }
+        return r;
+    }
+}
