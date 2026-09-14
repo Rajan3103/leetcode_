74eb752110256ac6200b9b2c872c39f52e9bd2d8
Karthig Rajan S <rajankarthig@gmail.com>
2026-09-14 15:29:20 +0530

    ✅ Longest Palindromic Substring | Medium | Python | Runtime: 8514 ms (5.97%) | Memory: 19.4 MB | 2026-09-14

--- medium/0005-longest-palindromic-substring/solution.py
+++ medium/0005-longest-palindromic-substring/solution.py
@@ -1,11 +1,11 @@
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        best = ""
-        for i in range(n):
-            for j in range(i, n):
-                sub = s[i:j+1]
-                if sub == sub[::-1]:
-                    if len(sub) > len(best):
-                        best = sub
-        return best
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        best = ""
+        for i in range(n):
+            for j in range(i, n):
+                sub = s[i:j+1]
+                if sub == sub[::-1]:
+                    if len(sub) > len(best):
+                        best = sub
+        return best
